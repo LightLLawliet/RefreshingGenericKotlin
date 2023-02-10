eda25f5a56b9ead9c9a34f9fdb3603ce20b583b9
NumpyGlumpy <99660044+NumpyGlumpy@users.noreply.github.com>
2023-02-10 17:05:10 +0300

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -1,44 +1,28 @@
-import sun.jvm.hotspot.oops.CellTypeState.value
-
-interface Save<T : Any> {
-    fun save(key: String, value: T)
+interface Y<X : Any, B : X> {
+    fun show(value: X): B
 }
 
-interface Read {
-    fun <T> read(key: String): T
+class StringSuperClass : Y<CharSequence, String> {
+    override fun show(value: CharSequence): String = ""
 }
 
-class SaveInt : Save<Int> {
-    override fun save(key: String, value: Int) {
-        TODO("Not yet implemented")
+data class ClassOne(private val a: String) {
+    fun <T> show(interactor: Interaction<T>): T {
+        return interactor.showString(a)
     }
 }
 
-class X(private val saveInt: Save<Int>) : Save<String> {
-
-    override fun save(key: String, value: String) {
-        saveInt.save(key, value.length)
+data class ClassTwo(private val x: Int) : Interaction<Int> {
+    override fun showString(a: String): Int {
+        for (i in 0..x) {
+            print(a)
+        }
+        return x
     }
 }
 
-class DataSource : Read, Save<Any> {
-
-    private val map = mutableMapOf<String, Any>()
-
-    override fun save(key: String, value: Any) {
-        map[key] = value
-    }
-
-    override fun <T> read(key: String): T {
-        return map[key] as T
+class ClassThree : Interaction<Double> {
+    override fun showString(a: String): Double {
+        return 0.9
     }
 }
-
-fun main() {
-    val datasource = DataSource()
-    datasource.save("txt", "value")
-    datasource.save("digit", 15)
-    val text: String = datasource.read("txt")
-    val int: Int = datasource.read("digit")
-}
-
