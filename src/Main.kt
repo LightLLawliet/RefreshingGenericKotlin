8568bc505498f56a8fc3f766a9c4e5065458a190
NumpyGlumpy <khaypov03@mail.ru>
2023-02-10 15:43:46 +0300

    -Commit 2

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,3 @@
-import sun.jvm.hotspot.oops.CellTypeState.value
-
 interface Save<T : Any> {
     fun save(key: String, value: T)
 }
@@ -34,11 +32,75 @@ class DataSource : Read, Save<Any> {
     }
 }
 
-fun main() {
-    val datasource = DataSource()
-    datasource.save("txt", "value")
-    datasource.save("digit", 15)
-    val text: String = datasource.read("txt")
-    val int: Int = datasource.read("digit")
+interface Y<X : Any, B : X> {
+    fun show(value: X): B
 }
 
+class StringSuperClass : Y<CharSequence, String> {
+    override fun show(value: CharSequence): String = ""
+}
+
+data class ClassOne(private val a: String) {
+    fun <T> show(interactor: Interaction<T>): T {
+        return interactor.showString(a)
+    }
+}
+
+data class ClassTwo(private val x: Int) : Interaction<Int> {
+    override fun showString(a: String): Int {
+        for (i in 0..x) {
+            print(a)
+        }
+        return x
+    }
+}
+
+class ClassThree : Interaction<Double> {
+    override fun showString(a: String): Double {
+        return 0.9
+    }
+}
+
+
+interface Interaction<T> {
+    fun showString(a: String): T
+}
+
+interface MyCustomObject {
+    interface Mapper<T> {
+        fun map(id: String): T
+
+        class Show : Mapper<Unit> {
+            override fun map(id: String) {
+                print(id)
+            }
+        }
+
+        class ToDomainObject : Mapper<DomainObject> {
+
+        }
+    }
+
+    fun <T> make(mapper: Mapper<T>): T
+
+    data class Base(private val id: String) : MyCustomObject {
+        override fun <T> make(mapper: Mapper<T>): T {
+            return mapper.map(id)
+        }
+    }
+}
+
+data class DomainObject(private val id: String, time: Long)
+
+fun main() {
+    val myCustomObject = MyCustomObject.Base("101")
+    myCustomObject.make(MyCustomObject.Mapper.Show())
+//    val datasource = DataSource()
+//    datasource.save("txt", "value")
+//    datasource.save("digit", 15)
+//    val text: String = datasource.read("txt")
+//    val int: Int = datasource.read("digit")
+//    val oneClass = ClassOne("a")
+//    val x: Int = oneClass.show(ClassTwo(1))
+//    val y: Double = oneClass.show(ClassThree())
+}
